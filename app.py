450d67ce07fdfbe993a444d18abe0ff21324e112
iberiaforestal <iberiaforestal.ing@gmail.com>
2025-11-17 11:05:33 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1762,7 +1762,7 @@ if submitted:
         else:
             # === 4. DEFINIR query_geom (UNA VEZ) ===
             if modo == "Por parcela":
-                query_geom = parcela.unary_union
+                query_geom = parcela.geometry.iloc[0]
             else:
                 query_geom = Point(x, y)
 
